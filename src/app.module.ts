8e99a82f9e260c22687d5df88caeba170169726a
Santiago Forero <sanfo0620@gmail.com>
2025-05-17 23:31:56 -0500

    book an library folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LibraryModule } from './library/library.module';
+import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [],
+  imports: [LibraryModule, BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
